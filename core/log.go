ee44b2661a61d901ce755be77459ac1a33c0d20b
Elyar <1595685558@qq.com>
2020-05-04 14:19:51 +0800

    add log

--- core/log.go
+++ core/log.go
@@ -1,11 +1,13 @@
 /*
-项目日志模块
+项目核心---日志模块
 需要实现的功能：
 
 1.以直观简洁的方式输出日志
 2.能够将日志保存到文件里
 3.能够将日志保存到数据库里
 4.能够日后对日志进行分析
+
+logrus六种日志级别：debug, info, warn, error, fatal, panic
 
 */
 
@@ -29,6 +31,10 @@ var (
 
 func main() {
 	logger := logrus.New()
-	logger.Out = os.Stdout
+	file, err := os.OpenFile("logrus.log", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		logger.Info("Faild to open log file")
+	}
+	logger.Out = file
 	logger.Warning("this is test")
 }
