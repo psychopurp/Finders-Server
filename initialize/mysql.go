dbfbf2a560e87fc062977d7ddbe8b6827a8dba7b
Elyar~ <1595685558@qq.com>
2020-07-15 11:20:11 +0800

    update

--- initialize/mysql.go
+++ initialize/mysql.go
@@ -2,10 +2,7 @@ package initialize
 
 import (
 	"finders-server/global"
-<<<<<<< HEAD
-=======
 	"time"
->>>>>>> test
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -21,11 +18,6 @@ func MySql() {
 		db.DB().SetMaxOpenConns(admin.MaxOpenConns)
 		// db.LogMode(global.CONFIG.MySQL.LogMode)
 		global.DB = db
-<<<<<<< HEAD
-	}
-
-}
-=======
 
 		// 取消注释后在更新和创建时会更新时间  更新时会更新时间
 		//db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
@@ -34,27 +26,25 @@ func MySql() {
 
 }
 
-
-func updateTimeStampForCreateCallback(scope *gorm.Scope){
-	if !scope.HasError(){
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
 		nowTime := time.Now().Unix()
-		if createTimeField, ok := scope.FieldByName("CreatedAt"); ok{
-			if createTimeField.IsBlank{
+		if createTimeField, ok := scope.FieldByName("CreatedAt"); ok {
+			if createTimeField.IsBlank {
 				createTimeField.Set(nowTime)
 			}
 		}
 
-		if updateTimeField, ok := scope.FieldByName("UpdatedAt"); ok{
-			if updateTimeField.IsBlank{
+		if updateTimeField, ok := scope.FieldByName("UpdatedAt"); ok {
+			if updateTimeField.IsBlank {
 				updateTimeField.Set(nowTime)
 			}
 		}
 	}
 }
 
-func updateTimeStampForUpdateCallback(scope *gorm.Scope){
-	if _, ok := scope.Get("gorm:update_column"); !ok{
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+	if _, ok := scope.Get("gorm:update_column"); !ok {
 		scope.SetColumn("UpdatedAt", time.Now().Unix())
 	}
 }
->>>>>>> test
